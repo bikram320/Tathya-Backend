c405ce7b6c8320d96313c91fef060842e168ea26
[Bikram Bk] <bkbikram727@gmail.com>
2025-11-08 12:33:43 +0545

    initial setup

---
+++ src/main/java/org/example/tathyabackend/TathyaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.tathyabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TathyaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TathyaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tathyabackend/TathyaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tathyabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TathyaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
